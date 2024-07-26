9c64e7ce1b2ef7975da750dcd9c90bb2a5c82675
twentynineteen <21161833+twentynineteen@users.noreply.github.com>
2024-07-26 10:14:11 +0100

    removed unused imports

--- src/main/java/dev/danmills/runners/Application.java
+++ src/main/java/dev/danmills/runners/Application.java
@@ -1,18 +1,12 @@
 package dev.danmills.runners;
 
-import dev.danmills.runners.Run.Location;
-import dev.danmills.runners.Run.Run;
-import dev.danmills.runners.Run.RunRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class Application {
